6d53e050a2e5e7754b5f027f7117637ed36c3501
demukeshchouhan <chouhan.mukesh17@gmail.com>
2018-02-14 15:05:45 +0530

    design updated

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var rename = require("gulp-rename");
 var connect = require('gulp-connect');
 var autoprefixer = require("autoprefixer");
 var postcss = require("gulp-postcss");
+var imagemin = require('gulp-imagemin');
+var imageminJpegRecompress = require('imagemin-jpeg-recompress');
 
 gulp.task('connect', function() {
 	connect.server({
@@ -31,11 +33,23 @@ gulp.task("sass", () => {
 });
 
 
-
+gulp.task("images", () => {
+	gulp.src('./img/*')
+	.pipe(imagemin({
+		use:[imageminJpegRecompress({
+		  loops:4,
+		  min: 50,
+		  max: 95,
+		  quality:'medium' 
+		})]
+	  }))
+        .pipe(gulp.dest('./img'))
+});
 
 
 gulp.task("watcher", () => {
-	gulp.watch("./scss/**/*.scss", ["sass"])
+	gulp.watch("./scss/**/*.scss", ["sass"]);
+	gulp.watch("./img/**/*", ["images"]);
 });
 
 
